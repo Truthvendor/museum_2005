de35f007f707be9d579270fa682b2ea043f5b925
Michael Chickillo <truthvendor@gmail.com>
2020-05-27 10:12:05 -0600

    Completed recommend_exhibit test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -28,6 +28,20 @@ class MuseumTest < Minitest::Test
   end
 
   def test_it_can_add_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
     assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_it_can_recommend_exhibits
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+# require "pry"; binding.pry
+    @patron_2.add_interest("IMAX")
+
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@patron_1)
+    assert_equal @imax, @dmns.recommend_exhibits(@patron_2)
   end
 end
