6d416362a31a8a53465afb71b47441182f41401b
Michael Chickillo <truthvendor@gmail.com>
2020-05-27 09:44:46 -0600

    Test created up to recommended_exhibits

---
+++ test/museum_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+
+class MuseumTest < Minitest::Test
+
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+  end
+
+  def test_it_exists_and_has_a_name
+    assert_instance_of Museum, @dmns
+    expected = "Denver Museum of Nature and Science"
+    assert_equal expected, @dmns.name
+  end
+
+  def test_it_starts_with_no_exhibits
+    assert_equal [], @dmns.exhibits
+  end
+
+  def test_it_can_add_exhibits
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+end
